d6f283e6582090ecbc44fc768a2b4da8d96d577b
ferdi yondemli <ferdi.yondemli@hmb.gov.tr>
2023-02-01 17:17:17 +0300

    improvement

--- src/main/java/com/ferdi/demo/user/repo/BookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ferdi.demo.user.repo;
-
-import com.ferdi.demo.user.entity.Book;
-import com.ferdi.demo.user.entity.Kullanici;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BookRepo extends JpaRepository<Book, Long> {
-}
--- src/main/java/com/ferdi/demo/user/repo/KullaniciRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ferdi.demo.user.repo;
-
-import com.ferdi.demo.user.entity.Kullanici;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-public interface KullaniciRepo extends JpaRepository<Kullanici, Long> {
-}
